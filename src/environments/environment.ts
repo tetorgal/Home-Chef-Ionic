e071341ccab142e0147fe38c47e9eddcfb74124d
DiegoB0 <potenciapanadera@gmail.com>
2024-07-24 18:09:20 -0600

    environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+    apiKey: 'AIzaSyD4iDpc52v9FwNPMfXJLmAJ9H7OUWu1LFA',
+    authDomain: 'home-chef-1faed.firebaseapp.com',
+    projectId: 'home-chef-1faed',
+    storageBucket: 'home-chef-1faed.appspot.com',
+    messagingSenderId: '865569262262',
+    appId: '1:865569262262:web:48628af80a14ad17637417',
+    measurementId: 'G-M4469MC4WQ',
   },
 };
